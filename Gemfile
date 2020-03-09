b90a9bfb69e349869fd8c5c38f0f125ba201b5f3
bruno <bruno.bertossi@gmail.com>
2020-03-09 16:33:04 -0300

    imagenes en active storage

--- Gemfile
+++ Gemfile
@@ -63,3 +63,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "image_processing", "~> 1.10"
+
+gem "aws-sdk-s3", "~> 1.60"
--- app/models/serie.rb
+++ app/models/serie.rb
@@ -1,5 +1,5 @@
 class Serie < ApplicationRecord
     has_and_belongs_to_many :lists
-    has_many :comments
-    has_many_attached :images
+    has_many :comments, dependent: :destroy
+    has_one_attached :images
 end
